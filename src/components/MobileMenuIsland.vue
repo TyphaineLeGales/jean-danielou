5f22ad836d6f048a5d2decb583de315516c96038
Typhaine Le Gales <typhaineLeGales@protonmail.com>
2026-06-01 11:16:35 +0200

    fix: mobile routing

--- src/components/MobileMenuIsland.vue
+++ src/components/MobileMenuIsland.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
     <IconBurgerMenu :open="menuOpen" @click="toggleMenuOpen" class="mt-2 size-12"/>
-    <MobileMenu :open="menuOpen" client:load />
+    <MobileMenu :open="menuOpen" :lang="lang" client:load />
   </div>
 </template>
 <script setup>
   import {ref} from 'vue'
   import IconBurgerMenu from './UI/IconBurgerMenu.vue';
   import MobileMenu from './MobileMenu.vue';
+  const props= defineProps({
+    lang: {
+      type: String
+    }
+  })
   const menuOpen = ref(false)
   const toggleMenuOpen =()=> menuOpen.value = !menuOpen.value
 </script>
